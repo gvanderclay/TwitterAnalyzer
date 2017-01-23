4d3c7c329def134b8f4a9db973f906c5ff262614
Gage Vander Clay <gvanderclay@yahoo.com>
2017-01-23 09:08:02 -0500

    add some comments

--- TwitterAnalysis/TweetHandler.py
+++ TwitterAnalysis/TweetHandler.py
@@ -1,13 +1,13 @@
 class TweetHandler:
     def __init__(self, conn):
+        # init sql connection
         self.conn = conn
         self.cursor = self.conn.cursor()
 
     def save_tweet(self, tweet):
-        print("INSERT INTO tweet_data VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-              tweet["id_str"], tweet["text"], tweet["created_at"],
-              tweet["favorite_count"], tweet["lang"], tweet["retweet_count"],
-              tweet["coordinates"], 0)
+        """
+        Save the tweet to the database
+        """
         self.cursor.execute(
             "INSERT INTO tweet_data VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
             (tweet["id_str"], tweet["text"], tweet["created_at"],
@@ -16,4 +16,8 @@ class TweetHandler:
         self.conn.commit()
 
     def filter_tweet(tweet):
+        """
+        Function that will decide what tweets to actually save
+        TODO decide how to filter tweets and actually use this function
+        """
         return True
--- TwitterAnalysis/TwitterStreamListener.py
+++ TwitterAnalysis/TwitterStreamListener.py
@@ -7,19 +7,23 @@ from TweetHandler import TweetHandler
 
 class TwitterStreamListener(tweepy.StreamListener):
     def __init__(self):
+        # grab db info
         db_info = yaml.safe_load(open("config/db.yml"))
         conn = sqlite3.connect(db_info["database_name"])
         self.tweet_handler = TweetHandler(conn)
 
     def on_data(self, data):
+        # TODO properly handle exceptions
         try:
             json_data = json.loads(data)
 
             self.tweet_handler.save_tweet(json_data)
             return True
         except KeyError as k:
+            print(k)
             pass
 
     def on_error(self, status_code):
+        # TODO properly handle http errors
         if status_code == 403:
             print("Limit is maybe reached")
